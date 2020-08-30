57809097b899ce9517645f467d266f033c9723ca
kamal kishore rajpurohit <kkraj16@gmail.com>
2020-08-30 16:11:45 +0530

    Spring boot initiate

---
+++ spring-jwt/src/test/java/com/learning/app/SpringJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
